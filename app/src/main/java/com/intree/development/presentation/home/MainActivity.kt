268bee4ec50e09afd5e5535204881d45df36674a
simonklejnstrup <sklejnstrup@gmail.com>
2022-04-01 12:30:15 +0200

    Initial commit

--- app/src/main/java/com/intree/development/presentation/home/MainActivity.kt
+++ app/src/main/java/com/intree/development/presentation/home/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.intree.development.presentation.home
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Rect
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.MotionEvent
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import androidx.annotation.DimenRes
+import androidx.annotation.DrawableRes
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.intree.development.R
 import com.intree.development.databinding.ActivityMainBinding
+import com.intree.development.presentation.home.inbox.InboxFragment
+import com.intree.development.presentation.home.introduce.IntroductionsFragment
+import com.intree.development.presentation.home.network.NetworkFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,51 +31,106 @@ class MainActivity : FragmentActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-        initNavController()
 
-        binding.bottomNavigationView.background = null
-        binding.bottomNavigationView.menu.getItem(2)?.isEnabled = false
-        binding.bottomNavigationView.menu.getItem(4)?.isChecked = true
+        val context = applicationContext
 
-        binding.fab.setOnClickListener {
-            val navHostFragment =
-                supportFragmentManager.findFragmentById(R.id.main_host_fragment) as NavHostFragment
-            binding.bottomNavigationView.setupWithNavController(navHostFragment.navController)
-            navHostFragment.navController.popBackStack()
-            navHostFragment.navController.navigate(R.id.fragmentNetwork)
+        val networkFragment = NetworkFragment()
+        val introductionsFragment = IntroductionsFragment()
+        val inboxFragment = InboxFragment()
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val bottomNavCenterIcon = findViewById<FloatingActionButton>(R.id.fab)
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.bottomTabNetwork -> {
+                    setCurrentFragment(networkFragment)
+                    bottomNavCenterIcon.setImageResource(R.drawable.ic_btn_nav_bar_center)
+                }
+                R.id.bottomTabIntroduce -> {
+                    setCurrentFragment(introductionsFragment)
+                    bottomNavCenterIcon.setImageResource(R.drawable.ic_btn_nav_bar_center)
+                }
+                R.id.bottomTabProfile -> {
+                    bottomNavCenterIcon.setImageResource(R.drawable.ic_btn_nav_bar_center)
+                }
+                R.id.bottomTabInbox -> {
+                    setCurrentFragment(inboxFragment)
+                    bottomNavCenterIcon.setImageResource(R.drawable.ic_btn_nav_bar_center)
+                }
+
+            }
+            true
         }
 
-        //_binding?.bottomNavigationView?.selectedItemId = R.id.bottomTabProfile
-        //bottomNavigationView.setOnNavigationItemSelectedListener(myNavigationItemListener)
-        //bottomNavigation.setupWithNavController(Navigation.findNavController(this, R.id.my_nav_host_fragment))
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val scaledBottomNavIconPressed = context.scaledDrawableResources(R.drawable.ic_btn_nav_bar_center_pressed, R.dimen.design_fab_image_size, R.dimen.design_fab_image_size)
+
+        bottomNavCenterIcon.setOnClickListener {
+            bottomNavigationView.selectedItemId = R.id.bottomTabExplore
+            bottomNavCenterIcon.setImageDrawable(scaledBottomNavIconPressed)
+        }
+
+        // Makes the status bar white TODO: Doesn't work
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
     }
 
-    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
-        if (event.action == MotionEvent.ACTION_DOWN) {
-            val v = currentFocus
-            if (v is EditText) {
+    private fun setCurrentFragment(fragment: Fragment) {
+        if (fragment != null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.main_host_fragment, fragment)
+            transaction.commit()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val bottomNavCenterIcon = findViewById<FloatingActionButton>(R.id.bottomNavigationView)
+        when (item.itemId) {
+            R.id.bottomTabNetwork -> bottomNavCenterIcon.setImageResource(R.drawable.ic_btn_nav_bar_center)
+            R.id.bottomTabIntroduce -> bottomNavCenterIcon.setImageResource(R.drawable.ic_btn_nav_bar_center)
+            R.id.bottomTabProfile -> bottomNavCenterIcon.setImageResource(R.drawable.ic_btn_nav_bar_center)
+            R.id.bottomTabInbox -> bottomNavCenterIcon.setImageResource(R.drawable.ic_btn_nav_bar_center)
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
+
+        if (event!!.action == MotionEvent.ACTION_DOWN) {
+            val view = currentFocus
+            if (view is EditText) {
                 val outRect = Rect()
-                v.getGlobalVisibleRect(outRect)
-                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
-                    v.clearFocus()
-                    val imm: InputMethodManager =
-                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
+                view.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event?.rawX.toInt(), event.rawY.toInt())) {
+                    view.clearFocus()
+                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(view.windowToken, 0)
                 }
             }
         }
         return super.dispatchTouchEvent(event)
     }
 
-    private fun initNavController() {
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.main_host_fragment) as NavHostFragment
-        binding.bottomNavigationView.setupWithNavController(navHostFragment.navController)
+    fun Context.scaledDrawableResources(@DrawableRes id: Int, @DimenRes width: Int, @DimenRes height: Int): Drawable {
+        val w = resources.getDimension(width).toInt()
+        val h = resources.getDimension(height).toInt()
+        return scaledDrawable(id, w, h)
+    }
+
+    fun Context.scaledDrawable(@DrawableRes id: Int, width: Int, height: Int): Drawable {
+        val bmp = BitmapFactory.decodeResource(resources, id)
+        val bmpScaled = Bitmap.createScaledBitmap(bmp, width, height, false)
+        return BitmapDrawable(resources, bmpScaled)
     }
 }
--- app/src/main/java/com/intree/development/presentation/home/network/NetworkFragment.kt
+++ app/src/main/java/com/intree/development/presentation/home/network/NetworkFragment.kt
@@ -1,37 +1,190 @@
 package com.intree.development.presentation.home.network
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.SearchView
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.intree.development.R
 import com.intree.development.databinding.FragmentNetworkBinding
+import com.intree.development.R
+import com.intree.development.domain.UserDataEntity
+import java.io.File
 
-class NetworkFragment : Fragment(R.layout.fragment_network) {
 
-    private lateinit var binding: FragmentNetworkBinding
+class NetworkFragment : Fragment(R.layout.fragment_network)  {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        if (!::binding.isInitialized) {
-            binding = FragmentNetworkBinding.inflate(inflater)
-        }
-        return binding.root
-    }
+
+    private lateinit var _binding: FragmentNetworkBinding
+    private lateinit var contactArrayList: ArrayList<UserDataEntity>
+
+    // Mock DB for filling out Recents-scrollview
+    val usernamesRecents = arrayOf("Amir Khan", "Art Peterson", "Janko Pascal", "Julian Joyce", "Imansyah XX", "Jessica Hansson", "Jon Buckley")
+    val userIconsMapRecents = mapOf("Amir Khan" to R.drawable.amir,"Art Peterson" to R.drawable.art, "Imansyah XX" to R.drawable.imansyah,"Janko Pascal" to R.drawable.janko, "Jessica Hansson" to R.drawable.jessica, "Jon Buckley" to R.drawable.jonathan, "Julian Joyce" to R.drawable.julian)
+
+    // Mock DB for filling out bottom-scrollview
+    val usernamesBottom = arrayOf("Cesar Rincon", "Clem Onojeghuo", "Hannah Busing", "Madison Lavern", "Mika laPetri", "Mike Clark", "Seth Doyle")
+    val userIconsMapBottom = mapOf("Cesar Rincon" to R.drawable.cesar_rincon, "Clem Onojeghuo" to R.drawable.clem_onojeghuo, "Hannah Busing" to R.drawable.hannah_busing, "Madison Lavern" to R.drawable.madison_lavern, "Mika laPetri" to R.drawable.mika_lapetri, "Mike Clark" to R.drawable.mike_clark, "Seth Doyle" to R.drawable.seth_doyle)
+    val connectionsMapBottom = mapOf("Cesar Rincon" to "By invite", "Clem Onojeghuo" to "Copenhagen, Denmark", "Hannah Busing" to "Kyiv, Ukraine", "Madison Lavern" to "Copenhagen, Denmark", "Mika laPetri" to "Madrid, Spain", "Mike Clark" to "Boston, Usa", "Seth Doyle" to "London, England")
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initOnClickListeners()
+
+        _binding = FragmentNetworkBinding.bind(view)
+
+        // Fills out the Recents-list with small cards
+        for (username in usernamesRecents) {
+            val imageDrawable = userIconsMapRecents.get(username)
+
+            if (imageDrawable != null) {
+                addSmallCard(username, imageDrawable)
+            }
+        }
+
+        // Fills out the bottom contactlist with wide cards
+        for (username in usernamesBottom) {
+            val imageDrawable = userIconsMapBottom.get(username)
+            val connection = connectionsMapBottom.get(username)
+
+            if (imageDrawable != null && connection != null) {
+                addWideCard(username, imageDrawable, connection)
+            }
+        }
+
+        // Adds 2 extra wide cards at the bottom to compensate for the view overflowing at the bottom
+        addWideCard("Intree", R.drawable.intree_logo, "")
+        addWideCard("Intree", R.drawable.intree_logo, "")
+
+
+        /**
+            Setup of search bar
+         */
+        var searchview = _binding.searchView
+
+        searchview.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(newText: String?): Boolean {
+                TODO("Not yet implemented")
+            }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // Cast cardContainer as ViewGroup to access removeAllViews()
+                val cardContainerVG = _binding.cardContainerBottom as ViewGroup
+
+                // Clear the content of bottom list
+                cardContainerVG.removeAllViews()
+
+                // Add transparent bar
+                val transparentBar = TextView(requireContext())
+
+                transparentBar.width = ViewGroup.LayoutParams.MATCH_PARENT
+                // Scale pixels to dp
+                val scale = context!!.resources.displayMetrics.density
+                val dp = 30
+                val pixels = (dp * scale + 0.5f)
+                transparentBar.height = pixels.toInt()
+                transparentBar.background = ResourcesCompat.getDrawable(resources, R.drawable.transparent_spacer, context?.theme)
+                cardContainerVG.addView(transparentBar)
+
+                //Filter List based on search criteria
+                var filteredUsernamesList = ArrayList<String>()
+                if (newText!= null) {
+                    filteredUsernamesList = usernamesBottom.filter { it.contains(newText, ignoreCase = true) } as ArrayList<String>
+
+                }
+
+                //Fills cardContainer with cards based on search criteria using UserDataEntity
+                /*
+                if (filteredUsernamesList != null) {
+                    for (i in 0 until filteredUsernamesList.size) {
+                        var user = UserDataEntity()
+                        user.fullName = filteredUsernamesList.get(i)
+                        val imageResource = userIconsMapBottom.get(name)
+                        val connection = connectionsMapBottom.get(name)
+
+                        //addWideCard(name, imageResource, connection)
+                    }
+                }
+                */
+
+                //Fill cardContainer with cards based on search criteria using mock DB
+                if (filteredUsernamesList != null) {
+                    for (name in filteredUsernamesList) {
+
+
+                        val imageResource = userIconsMapBottom.get(name)
+                        val connection = connectionsMapBottom.get(name)
+
+                        addWideCard(name, imageResource!!, connection!!)
+
+                    }
+                }
+                return false
+            }
+
+        })
+
+        // Transition to alternative fragment when qrIcon is pushed
+        /*
+        _binding.qrIcon.setOnClickListener{
+            val fragment = NetworkAltFragment()
+            val manager = parentFragmentManager
+            val transaction = manager.beginTransaction()
+            transaction.replace(
+                R.id.main_host_fragment, fragment
+            )
+            transaction.commit()
+
+        }
+        */
     }
 
-    private fun initOnClickListeners() {
-        binding.btnInvite.setOnClickListener {
-            findNavController().navigate(R.id.fragmentInvite)
-        }
+    private fun addSmallCard(name: String, imageResource: Int) {
+        val view = layoutInflater.inflate(R.layout.network_card_small, null)
+
+        val nameView = view.findViewById<TextView>(R.id.tv_name_smallcard)
+        val imageView = view.findViewById<ImageView>(R.id.iv_smallcard)
+
+        nameView.text = name
+        imageView.setImageResource(imageResource)
+
+        _binding.cardContainerRecents.addView(view)
     }
+
+
+    private fun addWideCard(name: String, imageResource: Int, connection: String) {
+        val view = layoutInflater.inflate(R.layout.network_card_wide, null)
+
+        val nameView = view.findViewById<TextView>(R.id.tvNameWideCard)
+        val imageView = view.findViewById<ImageView>(R.id.ivWidecard)
+        val connectionView = view.findViewById<TextView>(R.id.tvConnectedContentWideCard)
+
+        nameView.text = name
+        imageView.setImageResource(imageResource)
+        connectionView.text = connection
+
+        _binding.cardContainerBottom.addView(view)
+
+    }
+
+    // For possible future use
+    /*
+    private fun addWideCardWithUserDataEntity(user: UserDataEntity) {
+        val view = layoutInflater.inflate(R.layout.network_card_wide, null)
+
+        val nameView = view.findViewById<TextView>(R.id.tvNameWideCard)
+        val imageView = view.findViewById<ImageView>(R.id.ivWidecard)
+        val connectionView = view.findViewById<TextView>(R.id.tvConnectedContentWideCard)
+
+        nameView.text = user.fullName
+        imageView.setImageBitmap(user.photo.photo)
+        connectionView.text = user.metIn.toString()
+
+        _binding.cardContainerBottom.addView(view)
+
+    }
+    +*/
 }
